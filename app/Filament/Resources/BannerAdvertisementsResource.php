424b47b020751c8dcb2c3d517010600b77bd9aa4
Samuel Yahya <samuelyahya25@gmail.com>
2024-12-29 19:36:54 +0700

    first commit

---
+++ app/Filament/Resources/BannerAdvertisementsResource.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BannerAdvertisementsResource\Pages;
+use App\Filament\Resources\BannerAdvertisementsResource\RelationManagers;
+use App\Models\BannerAdvertisements;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BannerAdvertisementsResource extends Resource
+{
+    protected static ?string $model = BannerAdvertisements::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('link')->activeUrl()->required()->maxLength(255),
+
+                FileUpload::make('thumbnail')->required()->image(),
+                
+                Select::make('is_active')->options([
+                    'active' => 'Active',
+                    'not_active' => 'Not Active'
+                ])->required(),
+                
+                Select::make('type')->options([
+                    'banner' => 'Banner',
+                    'square' => 'Square'
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('link')->searchable(),
+
+                TextColumn::make('is_active')
+                ->badge()
+                ->color(fn (string $state): string => match ($state) {
+                    'active' => 'success',
+                    'not_active' => 'danger',
+                }),
+
+                ImageColumn::make('thumbnail')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListBannerAdvertisements::route('/'),
+            'create' => Pages\CreateBannerAdvertisements::route('/create'),
+            'edit' => Pages\EditBannerAdvertisements::route('/{record}/edit'),
+        ];
+    }
+}
